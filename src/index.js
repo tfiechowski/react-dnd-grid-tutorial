55b5fcbf60505764df4720c2a66f7e6971fe8c32
Tomasz Fiechowski <tomasz.fiechowski@gmail.com>
2019-10-30 09:12:08 +0100

    Setup Dnd and Grid Providers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { DndProvider } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { GridProvider } from "./GridContext";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <DndProvider backend={HTML5Backend}>
+    <GridProvider>
+      <App />
+    </GridProvider>
+  </DndProvider>,
+  document.getElementById("root")
+);
