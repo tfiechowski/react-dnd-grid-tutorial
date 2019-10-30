2f7709f659fe71575d7077d855630545636f653e
Tomasz Fiechowski <tomasz.fiechowski@gmail.com>
2019-10-30 09:12:26 +0100

    Show example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+import DragItem from "./DragItem";
+import { Grid, GridImage, GridItem } from "./Grid";
+import GridContext from "./GridContext";
 
 function App() {
+  const { items, moveItem } = useContext(GridContext);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid>
+        {items.map(item => (
+          <DragItem key={item.id} id={item.id} onMoveItem={moveItem}>
+            <GridItem>
+              <GridImage src={item.src}></GridImage>
+            </GridItem>
+          </DragItem>
+        ))}
+      </Grid>
     </div>
   );
 }
